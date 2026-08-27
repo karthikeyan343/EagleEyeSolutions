34bb3fca481ea3f1ed735a507b6463a13732fe7c
karthikeyan343 <karthikeyanshankar2004@gmail.com>
2026-08-27 10:53:38 +0530

    Folder Structure created

---
+++ src/Components/WrapperedComponents/PageLayout.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import NavBar from '../SpecifiedComponents/NavBar'
+import Hero from '../SpecifiedComponents/Hero'
+import Service from '../SpecifiedComponents/Service'
+import AboutUs from '../SpecifiedComponents/AboutUs'
+import FAQ from '../SpecifiedComponents/FAQ'
+import Contact from '../SpecifiedComponents/Contact'
+import Footer from '../SpecifiedComponents/Footer'
+
+const PageLayout = () => {
+  return (
+    <>
+    <NavBar/>
+    <Hero/>
+    <Service/>
+    <AboutUs/>
+    <FAQ/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default PageLayout
---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageLayout from '../Components/WrapperedComponents/PageLayout'
+
+const MainPage = () => {
+  return (
+    <>
+      <PageLayout/>
+    </>
+  )
+}
+
+export default MainPage
