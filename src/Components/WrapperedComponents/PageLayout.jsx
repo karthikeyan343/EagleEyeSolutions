c4931b6c523cc303799d07f57b73b933ef8e1371
karthikeyan343 <karthikeyanshankar2004@gmail.com>
2026-08-28 12:15:31 +0530

    profuct comp ad

--- src/Components/WrapperedComponents/PageLayout.jsx
+++ src/Components/WrapperedComponents/PageLayout.jsx
@@ -3,19 +3,19 @@ import NavBar from '../SpecifiedComponents/NavBar'
 import Hero from '../SpecifiedComponents/Hero'
 import Service from '../SpecifiedComponents/Service'
 import AboutUs from '../SpecifiedComponents/AboutUs'
-import FAQ from '../SpecifiedComponents/FAQ'
 import Contact from '../SpecifiedComponents/Contact'
 import Footer from '../SpecifiedComponents/Footer'
+import Products from '../SpecifiedComponents/Products'
 
 const PageLayout = () => {
   return (
     <>
     <NavBar/>
     <Hero/> 
+    <Products/>
     <Service/>
-    <AboutUs/>
-    <FAQ/>
     <Contact />
+     <AboutUs/>
     <Footer />
     </>
   )
